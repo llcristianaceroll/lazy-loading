0d6f97b2fd2095b8ef180f6e187074076005c3c7
Cristian Acero <llcristianaceroll@gmail.com>
2022-07-01 16:06:08 -0500

    lazy loading added

--- src/lazy.js
+++ src/lazy.js
@@ -3,13 +3,20 @@ const isIntersecting = (entry) => {
     return entry.isIntersecting;
 }
 
-const action = (entry) => {
-    const nodo = entry.target;
-    console.log('hola');
-    observer.unobserve(nodo)
+const loadImage = (entry) => {
+    const container = entry.target;
+    const image = container.firstChild;
+    const url = image.dataset.src
+    image.src= url;
+
+    // const image = container.querySelector('img')
+    // image.src= image.dataset.src;
+    // console.log(nodo.nodeName);
+    observer.unobserve(container)
+
 }
 const observer = new IntersectionObserver((entries) => {
-    entries.filter(isIntersecting).forEach(action);
+    entries.filter(isIntersecting).forEach(loadImage);
 })
 
 export const registerImage = (image) => {
