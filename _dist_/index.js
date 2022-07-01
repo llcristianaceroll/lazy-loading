7efdda618dc5f80285b1adb94c5eb4f294752701
llcristianaceroll <llcristianaceroll@users.noreply.github.com>
2022-07-01 16:21:00 +0000

    deploy: 04da4c7a8a2d5882ee654a17e22be035fab17723

--- _dist_/index.js
+++ _dist_/index.js
@@ -2,5 +2,35 @@
  * This file is just a silly example to show everything working in the browser.
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
+const maximum = 123;
+const minimum = 1;
 
-console.log('Happy hacking :)')
+const random = () => Math.floor(Math.random() * (maximum - minimum)) - minimum
+
+const createImageNode = () =>  {
+    const container = document.createElement('div');
+    container.className = 'p-4'
+
+    const image = document.createElement('img');
+    image.className = 'mx-auto';
+    image.src = `https://randomfox.ca/images/${random()}.jpg`;
+    image.width ='320';
+
+    container.appendChild(image);
+
+    return container; 
+}
+
+const newImage = createImageNode();
+const mountNode = document.getElementById('images');
+
+// mountNode.appendChild(newImage);
+mountNode.append(newImage);
+
+const addButton = document.querySelector('button');
+const action = () => {
+    const newImage = createImageNode();
+    mountNode.append(newImage);
+}
+
+addButton.addEventListener('click', action)
